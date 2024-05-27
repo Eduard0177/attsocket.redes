4a67c5192a448d334a88742710ce148b9e321b93
Eduard0177 <131684798+Eduard0177@users.noreply.github.com>
2024-05-27 09:13:16 -0300

    Update client.py

--- socket.py/client.py
+++ socket.py/client.py
@@ -1,12 +1,47 @@
-import socket 
+import socket
+import threading
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HEADER = 64
+PORT = 5050
+FORMAT = 'UTF-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "10.1.8.70"
 
-cliente.connect('localhost', 8888)
+ADDR = (SERVER, PORT)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-terminado = False   
-print('digite tt para terminar o chat')
+client.connect(ADDR)
 
-while not terminado:
-    cliente.sec(input('Mensagem: '.encode('utf-8')) )
-                
+
+def receive():
+    while True:
+        try:
+            msg = client.recv(2048).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                print("Desconectado pelo servidor")
+                break
+            print(msg + '\n')
+        except Exception as e:
+            print(f"Erro ao receber mensagem: {e}")
+            break
+
+
+def send(msg):
+    try:
+        client.send(msg.encode(FORMAT))
+    except Exception as e:
+        print(f"Erro ao enviar mensagem: {e}")
+
+
+thread1 = threading.Thread(target=receive)
+thread1.start()
+
+while True:
+    msg = input()
+    if msg == DISCONNECT_MESSAGE:
+        send(msg)
+        break
+    send(msg)
+
+thread1.join()
+client.close()
